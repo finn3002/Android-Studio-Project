8d993b0edae2550daebf0e2a302e184404135f36
sherryWang <sherry5920914@gmail.com>
2020-09-01 22:30:00 +0800

    Revert "Merge pull request #3 from finn3002/sherry"
    This reverts commit db9f9c95378becd17c74c6e2c4a743e2d379caec, reversing
    changes made to 2ffa649b02c233e70c78b31e7a40ac4a18e047b4.

---
+++ app/src/main/java/com/team/team_project/foodBean.java
@@ -0,0 +1,93 @@
+package com.team.team_project;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+public class foodBean {
+
+    private String foodnm;
+    private Double food_protein;
+    private int food_price;
+    private Double food_fat;
+    private Double food_calorie;
+    private Double food_carbon;
+    private Timestamp keyin;
+    foodBean(){
+        this.foodnm="";
+        this.food_protein=0.0;
+        this.food_price=0;
+        this.food_fat=0.0;
+        this.food_calorie=0.0;
+        this.food_carbon=0.0;
+        this.keyin=Timestamp.valueOf("2020-01-01 00:00:00");
+    }
+
+    public String getFoodnm() {
+        return foodnm;
+    }
+
+    public void setFoodnm(String foodnm) {
+        this.foodnm = foodnm;
+    }
+
+    public Double getFood_protein() {
+        return food_protein;
+    }
+
+    public void setFood_protein(Double food_protein) {
+        this.food_protein = food_protein;
+    }
+
+    public int getFood_price() {
+        return food_price;
+    }
+
+    public void setFood_price(int food_price) {
+        this.food_price = food_price;
+    }
+
+    public Double getFood_fat() {
+        return food_fat;
+    }
+
+    public void setFood_fat(Double food_fat) {
+        this.food_fat = food_fat;
+    }
+
+    public Double getFood_calorie() {
+        return food_calorie;
+    }
+
+    public void setFood_calorie(Double food_calorie) {
+        this.food_calorie = food_calorie;
+    }
+
+    public Double getFood_carbon() {
+        return food_carbon;
+    }
+
+    public void setFood_carbon(Double food_carbon) {
+        this.food_carbon = food_carbon;
+    }
+
+    public Timestamp getKeyin() {
+        return keyin;
+    }
+
+    public void setKeyin(Timestamp keyin) {
+        this.keyin = keyin;
+    }
+
+    @Override
+    public String toString() {
+        return "foodBean{" +
+                "foodnm='" + foodnm + '\'' +
+                ", food_protein=" + food_protein +
+                ", food_price=" + food_price +
+                ", food_fat=" + food_fat +
+                ", food_calorie=" + food_calorie +
+                ", food_carbon=" + food_carbon +
+                ", keyin=" + keyin +
+                '}';
+    }
+}
